8ae6b24c4c60262131ffd6d18323f8e7e2eed1af
Tobia Gasparoni <44105796+tobiaGasparoni@users.noreply.github.com>
2025-02-01 22:23:04 -0500

    make event type explicit

--- amplify/functions/my-first-function/handler.ts
+++ amplify/functions/my-first-function/handler.ts
@@ -1,3 +1,6 @@
-export const handler = async (event) => {
+import { APIGatewayEvent } from "aws-lambda";
+
+
+export const handler = async (event: APIGatewayEvent) => {
   return "Hello from my first function!";
 };
