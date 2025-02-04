7ae69ab90ae5af0d5bd6cd6eaaf4db9a886c58b5
Tobia Gasparoni <44105796+tobiaGasparoni@users.noreply.github.com>
2025-02-04 16:05:13 -0500

    remove unused import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react'
 import { Dialog, DialogPanel } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
-import { ChevronRightIcon } from '@heroicons/react/20/solid'
 
 const navigation = [
   { name: 'Product', href: '#' },
